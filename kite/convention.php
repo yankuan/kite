34201ad6460d9971d0e37f55777d22622a08f573
yankuan <1026431088@qq.com>
2017-03-18 00:13:51 +0800

    优化配置

--- kite/convention.php
+++ kite/convention.php
@@ -9,14 +9,10 @@ return [
     'app_namespace'          => 'app',
     // 应用调试模式
     'app_debug'              => true,
-    // 应用Trace
-    'app_trace'              => false,
     // 应用模式状态
     'app_status'             => '',
     // 是否支持多模块
     'app_multi_module'       => true,
-    // 入口自动绑定模块
-    'auto_bind_module'       => false,
     // 注册的根命名空间
     'root_namespace'         => [],
     // 扩展函数文件
@@ -25,12 +21,8 @@ return [
     'default_return_type'    => 'html',
     // 默认时区
     'default_timezone'       => 'PRC',
-    // 是否开启多语言
-    'lang_switch_on'         => false,
     // 默认全局过滤方法 用逗号分隔多个
     'default_filter'         => '',
-    // 默认语言
-    'default_lang'           => 'zh-cn',
     // 应用类库后缀
     'class_suffix'           => false,
     // 控制器类后缀
